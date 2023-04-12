76d18ea5c97909d9d83dba250db463f6d5d0a6e9
Arya <aryapratimray@gmail.com>
2023-04-12 10:03:54 +0530

    bug fixes

--- employees.js
+++ employees.js
@@ -9,25 +9,22 @@ db.connect()
 
 router.post('/employees', (req, res) => {
     try {
-        const { employee_id, name, email, phone } = req.body;
+        const { employee_id, name, job, email, phone, address, city, state } = req.body;
 
         db.query(
-            'INSERT INTO employees (employee_id, name, email, phone) VALUES (?, ?, ?, ?)',
-            [employee_id, name, email, phone]
+            'INSERT INTO employees (employee_id, name, email, phone, address, city, state) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [employee_id, job, name, email, phone, address, city, state]
         );
 
-        // const employeeId = result1.insertId;
-
-        // const [result2] = db.query(
-        //     'INSERT INTO employee_contacts (employee_id, email, phone) VALUES (?, ?, ?)',
-        //     [employeeId, email, phone]
-        // );
-
         res.status(201).json({
-            id: employee_id,
+            employee_id,
+            job,
             name,
             email,
             phone,
+            address,
+            city, 
+            state
         });
     } catch (err) {
         console.error(err);
@@ -45,7 +42,7 @@ router.get('/employees', (req, res) => {
         const offset = (page - 1) * limit;
 
         db.query(
-            'SELECT e.employee_id, e.name, e.email, e.phone FROM employees e',function(error,result){
+            'SELECT e.employee_id, e.name, e.job, e.email, e.phone, e.address, e.city, e.state FROM employees e ORDER BY e.employee_id LIMIT ? OFFSET ?',[limit, offset],function(error,result){
                 if(error) throw error;
                 res.json(result)
             }
@@ -61,12 +58,12 @@ router.get('/employees', (req, res) => {
 
 router.put('/employees/:id', (req, res) => {
     try {
-        const { name, email, phone } = req.body;
+        const { name, job, email, phone, address, city, state } = req.body;
         const { id } = req.params;
 
         db.query(
-            'UPDATE employees SET name = ?, email = ?, phone = ? WHERE employee_id = ?',
-            [name, email, phone, id],function(error,result){
+            'UPDATE employees SET name = ?, job = ?, email = ?, phone = ?, address = ?, city = ?, state = ? WHERE employee_id = ?',
+            [name, job, email, phone, address, city, state, id],function(error,result){
                 if (result.affectedRows === 0) {
                     return res.status(404).json({ error: 'Employee not found' });
                 }
@@ -114,7 +111,7 @@ router.get('/employees/:id', (req, res) => {
         const { id } = req.params;
 
         db.query(
-            'SELECT e.employee_id, e.name, e.email, e.phone FROM employees e WHERE e.employee_id = ?',
+            'SELECT e.employee_id, e.name, e.job, e.email, e.phone, e.address, e.city, e.state FROM employees e WHERE e.employee_id = ?',
             [id],function(error,result){
                 if (result.length === 0) {
                     return res.status(404).json({ error: 'Employee not found' });
